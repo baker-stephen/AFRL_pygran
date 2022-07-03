2e2386376c65546e1056f5406e7c2642fe1989d6
Stephen Baker <stephenbaker@pacbell.net>
2022-07-03 10:33:36 -0700

    Finish 1.5" ID 7/16" DP first run.

--- convert_vti.py
+++ convert_vti.py
@@ -11,13 +11,13 @@ def output_vti(postions:list,tstep:int):
 
 
 if __name__ == "__main__":
-    atom_count = 50
+    atom_count = 300
     box_bounds = []
-    for step in range(10000, 12890001, 10000):
+    for step in range(50000, 9950001, 50000):
         positions = []
         if step%100000==0:
             print("step: ",step)
-        with open('out-SpringDashpot-17:34:49-12.6.2022/traj/particles'+str(step)+'.vtk', 'r') as read:
+        with open('out-SpringDashpot-9:26:45-3.7.2022/traj/particles'+str(step)+'.vtk', 'r') as read:
 
             this_atom_count = 0
             for i in range(9):
