0fb99edc4f575c7c41c159c9f713225562980c57
Stephen Baker <sbaker@lclark.edu>
2022-07-16 17:58:04 -0700

    Setup and fix even more pygran runs

--- one_quarter/0_pt_9_DP/one_quarter.py
+++ one_quarter/0_pt_9_DP/one_quarter.py
@@ -7,10 +7,10 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    total_parts = 2000 #TODO: change total particles
+    total_parts = 1500
     num_insertions = 1
     parts_per_insert = total_parts//num_insertions
-    dp_in = 15/64 #TODO: change particle diameter
+    dp_in = 0.9/25.4
     # Create a dictionary of physical parameters
     params = {
 
@@ -19,15 +19,15 @@ if __name__ == "__main__":
         # z bound given by funnel height (2"/sqrt(2) = 35.921mm) + pipe height (1.2"=30.48mm) + extra insertion room
         # x and y bounds given by funnel OR (funnel height) + pipe OR (.375"/2 = 4.7625mm) = 40.68mm
         #'box': (-22, 22, -22, 22, -3, 75),  # simulation box size mm
-         'box': (-4, 4, -4, 4, -1.1, 17),  # TODO: simulation box size in inches
+         'box': (-0.6, 0.6, -0.6, 0.6, -.5, 5),  # simulation box size in inches
         # Define component(s)
         # Dp mini = 1mm, r = .5mm = .0198505"
         'species': (
-            {'material': glass, 'style': 'sphere', 'radius': dp_in/2},),
+            {'material': glass, 'style': 'sphere', 'radius': dp_in/2},), #TODO: change this for different Dp
 
         # Set skin distance to be 1/4 particle diameter
         # 'nns_skin': .25e-3,
-        'nns_skin': dp_in/4,
+        'nns_skin': dp_in/4, #TODO: change this for different Dp
 
         # Timestep
         # Needs to be reduced to satisfy rayleigh time constraint, apparently dependent on particle size
@@ -38,7 +38,7 @@ if __name__ == "__main__":
          'gravity': (385.827, 0, 0, -1),
 
         # Setup I/O
-        'traj': {'pfile': 'particles*.vtk', 'mfile': 'pipe*.vtk', 'freq': 50000},
+        'traj': {'pfile': 'particles*.vtk', 'mfile': 'pipe*.vtk', 'freq': 10000},
 
         # Stage runs [optional]
         'stages': {'insertion': 2.3e6},
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         # Used meshlab to reduce mesh count by .9, still needed curvature tolerance
         # TODO: define PVC material
         'mesh': {
-            'pipe': {'file': '../../mesh/three_half_in_pg_reduced_smooth.stl', 'mtype': 'mesh/surface/stress', 'material': steel,
+            'pipe': {'file': '../../mesh/one_quarter_funnel_closed.stl', 'mtype': 'mesh/surface/stress', 'material': steel,
                      'args': {'curvature_tolerant': 'yes'}
                      },
         },
@@ -59,17 +59,16 @@ if __name__ == "__main__":
     # Create an instance of the DEM class
     sim = simulation.DEM(**params)
 
+
     air_resistance = sim.addViscous(species=1, gamma=0.1)
 
-    # particle insertion
-    # My best guess for cylinder numbers: x0, y0, r, z_min, z_max
-    # Minimum height of insert cylinder of rad .5": 12.2mm above top of pipe, or z = 43.18
-    #TODO: change range back from 1 to num_insertions and value from 10 to parts_per_insert
+
     for i in range(num_insertions):
         # insert = sim.insert(species=1, value=parts_per_insert, region=('cylinder', 'z', 0, 0, 12.2e-3, 45e-3, 85e-3),
         #                     args={'orientation': 'random'})
-        insert = sim.insert(species=1, value=parts_per_insert, region=('cylinder', 'z', 0, 0, 3, 12, 15),
+        insert = sim.insert(species=1, value=parts_per_insert, region=('cylinder', 'z', 0, 0, 3.8, 9, 13.5),
                             args={'orientation': 'random'})
+
 
         # Run insertion stage
         sim.run(params['stages']['insertion']*2, params['dt'])
