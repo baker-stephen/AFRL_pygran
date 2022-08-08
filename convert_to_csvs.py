2e5c76e2751293b8c4b2894e5a997232fe3b1d73
Stephen Baker <sbaker@lclark.edu>
2022-08-08 13:01:16 -0700

    Should be a good fix for csv convert

--- convert_to_csvs.py
+++ convert_to_csvs.py
@@ -2,8 +2,8 @@ import os
 
 #TODO: Comment this file
 
-def output_csv(postions: list, tstep: int):
-    with open('csvs/particles' + str(tstep) + '.csv', 'w') as write:
+def output_csv(postions: list, tstep: int, base: str):
+    with open(base+'csvs/particles' + str(tstep) + '.csv', 'w') as write:
         write.write("x,y,z\n")
         for part in range(len(postions)):
             if len(postions[part]) < 3:
@@ -14,11 +14,13 @@ def output_csv(postions: list, tstep: int):
 
 
 def go(atom_count: int, final_step: int, source_dir: str):
+
+    base_dir = source_dir[:source_dir.rindex("/")+1]
     try:
-        os.makedirs(source_dir + '/csvs/', exist_ok=True)
-        print("Directory '/csvs/' created successfully\n")
+        os.makedirs(base_dir + '/csvs/', exist_ok=True)
+        print("Directory '%s' created successfully\n" % (base_dir+'/csvs'))
     except OSError as error:
-        print("Directory '/csvs/' can not be created. Error: %s\n" % str(error))
+        print("Directory '%s' can not be created. Error: %s\n" % (base_dir+'/csvs',str(error)))
     box_bounds = []
     for step in range(50000, final_step, 50000):
         positions = []
@@ -42,7 +44,7 @@ def go(atom_count: int, final_step: int, source_dir: str):
                     positions[i].append(float(data[4]))
                 else:
                     positions.append([box_bounds[0][0], box_bounds[1][0], box_bounds[2][0]])
-            output_csv(positions, step)
+            output_csv(positions, step, base_dir)
             read.close()
 
     print("Done converting to csvs")
