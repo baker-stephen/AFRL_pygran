2414754198e5fea4f2a0b5da1030c313827735c3
Stephen Baker <stephenbaker@pacbell.net>
2022-09-02 12:45:16 -0700

    0.602 updates

--- outputs/0pt602/7_16/convert_vti.py
+++ outputs/0pt602/7_16/convert_vti.py
@@ -1,6 +1,6 @@
 
 def output_vti(postions:list,tstep:int):
-    with open('csvs_new/particles'+str(tstep)+'.csv','w') as write:
+    with open('csvs/particles'+str(tstep)+'.csv','w') as write:
         write.write("x,y,z\n")
         for part in range(len(postions)):
             if len(postions[part]) < 3:
@@ -11,13 +11,13 @@ def output_vti(postions:list,tstep:int):
 
 
 if __name__ == "__main__":
-    atom_count = 50
+    atom_count = 96
     box_bounds = []
     for step in range(50000, 1000000001, 50000):
         positions = []
         if step%100000==0:
             print("step: ",step)
-        with open('9-22-8_29-7-2022/traj/particles'+str(step)+'.vtk', 'r') as read:
+        with open('sim_out_14:12:42_8-8-2022/traj/particles'+str(step)+'.vtk', 'r') as read:
 
             this_atom_count = 0
             for i in range(9):
