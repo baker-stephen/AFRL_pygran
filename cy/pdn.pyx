f2401c207913f140e5b25799eff3f8d99ca4f966
Stephen Baker <sbaker@lclark.edu>
2022-11-02 16:48:21 -0700

    Fix pdn again

--- cy/pdn.pyx
+++ cy/pdn.pyx
@@ -11,7 +11,7 @@ def go(params: PD):
     print("Parameters received:")
     #Since output is only written every 50000 steps, the final recorded position of the particles can be found via:
     final_step_out = params.final_step() - (params.final_step() % 50000)
-    import_csv = '../'+params.out_dir+'particles'+str(final_step_out)+'.csv'
+    import_csv = '../'+params.out_dir+'particles/csvs/'+str(final_step_out)+'.csv'
     print(import_csv)
     Dp = params.DP
     print(Dp)
