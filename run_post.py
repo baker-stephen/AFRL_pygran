6744662709154c2c6e7e0ed6052eb9e19977f4ed
Stephen Baker <sbaker@lclark.edu>
2022-11-02 16:36:44 -0700

    Add run post

---
+++ run_post.py
@@ -0,0 +1,57 @@
+import sys
+
+import convert_to_csvs
+import cy.run_post as rp
+
+from param_defn import PD
+
+if __name__ == "__main__":
+
+    # Retrieve command-line arguments. First element is always file name, we can skip that.
+    args = sys.argv[1:]
+    print('args:', args)
+
+    if len(args) != 2 and len(args) != 3:
+        raise Exception("Please specify both ID and DP")
+
+    # Define the parameters of the simulation
+    if len(args) == 3:
+        num_insertions = int(str(args[2]).strip())
+        params = PD(str(args[0]).strip(), str(args[1]).strip(), num_inserts=num_insertions)
+    else:
+        params = PD(str(args[0]).strip(), str(args[1]).strip())
+
+    # Initialize the output directory
+    # params.output_dir()
+
+    params.out_dir = 'outputs/0pt602/2pt8_25pt4/sim_out_10:58:40_30-10-2022'
+
+    wrt_dir = params.out_dir[:params.out_dir.rfind('/') + 1]
+
+    print("write directory:", wrt_dir)
+
+    sys.stdout = open(wrt_dir + 'stdout.txt', 'w')
+    sys.stderr = open(wrt_dir + 'stderr.txt', 'w')
+
+    print("out_dir: %s" % params.out_dir)
+
+    originalOutDir = params.out_dir
+
+    # Run the simulation
+    # run_sim.go(params)
+
+    print("\n\nSimulation Complete!\n\n")
+
+    print("now out_dir: %s" % params.out_dir)
+
+    print("OG out_dir: %s" % originalOutDir)
+
+
+    # Convert the output to Paraview-compatible and readable csvs
+    convert_to_csvs.go(params.N_spheres(), params.final_step(), originalOutDir)  # TODO: test new csvs method
+
+    print("\nAll Pygran steps complete")
+
+    rp.go(params)
+
+    print("\n\nRun Completed!!")
