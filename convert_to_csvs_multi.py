8449b3ac84712ed2bbeaeaca9321276bde052d74
Stephen Baker <stephenbaker@pacbell.net>
2022-11-16 14:10:33 -0800

    Prepare for a couple more convert to csvs
    Signed-off-by: Stephen Baker <stephenbaker@pacbell.net>

--- convert_to_csvs_multi.py
+++ convert_to_csvs_multi.py
@@ -19,32 +19,40 @@ Last 1.59 3/16
 Missed 0.602 1/16
 """
 
-ID_DP_dict_all = {'0.26': ['0.7/25.4', '0.8/25.4', '0.9/25.4', '0.85/25.4', '1/25.4', '1.1/25.4', '1.2/25.4', '1.5/25.4', '1.7/25.4'],
-              '0.602': ['1/4', '1/8', '1/16', '3/16', '7/16', '2.8/25.4', '3/32'],
-              '1.029': ['1/4', '1/8', '3/16', '3/32', '5/16', '7/16', '15/32', '5/32',],
-              '1.59': ['1/4', '1/8', '3/16', '5/16', '5/32', '7/16', '7/32', '9/32', '9/64', '15/64']}
+ID_DP_dict_all = {
+    '0.26': ['0.7/25.4', '0.8/25.4', '0.9/25.4', '0.85/25.4', '1/25.4', '1.1/25.4', '1.2/25.4', '1.5/25.4', '1.7/25.4'],
+    '0.602': ['1/4', '1/8', '1/16', '3/16', '7/16', '2.8/25.4', '3/32'],
+    '1.029': ['1/4', '1/8', '3/16', '3/32', '5/16', '7/16', '15/32', '5/32', ],
+    '1.59': ['1/4', '1/8', '3/16', '5/16', '5/32', '7/16', '7/32', '9/32', '9/64', '15/64']}
 
-ID_DP_dict = {'0.26': ['0.7/25.4',],
-              '0.602': ['2.8/25.4', '3/32'],
-              '1.029': ['1/8', '3/32', '5/32',],
-              '1.59': ['1/4', '1/8', '3/16',]}
+
+
+# ID_DP_dict = {'0.26': ['0.7/25.4', ],
+#               '0.602': ['2.8/25.4', '3/32'],
+#               '1.029': ['1/8', '3/32', '5/32', ],
+#               '1.59': ['1/4', '1/8', '3/16', ]}
+
+ID_DP_dict = {
+              '0.602': ['1/8', '1/16', '3/32'],
+              '1.029': ['5/32'],}
+
 
 if __name__ == "__main__":
     base_dir = 'outputs/'
     for ID_str in ID_DP_dict.keys():
-        print("ID:",ID_str)
-        ID_dir = base_dir+ID_str.replace('.','pt')+'/'
+        print("ID:", ID_str)
+        ID_dir = base_dir + ID_str.replace('.', 'pt') + '/'
         for Dp_str in ID_DP_dict[ID_str]:
-            params = PD(ID_str,Dp_str,num_inserts=10)
-            print("Dp:",Dp_str)
+            params = PD(ID_str, Dp_str, num_inserts=10)
+            print("Dp:", Dp_str)
             # Dp_str = Dp_str.replace('.', 'pt').replace('/', '_')
-            out_dir = ID_dir + Dp_str.replace('.', 'pt').replace('/', '_')+'/'
-            dirs = glob(out_dir+'*/')
+            out_dir = ID_dir + Dp_str.replace('.', 'pt').replace('/', '_') + '/'
+            dirs = glob(out_dir + '*/')
             for dir in dirs:
-                dir_check = dir[len(out_dir):len(dir)-1]
+                dir_check = dir[len(out_dir):len(dir) - 1]
                 print(dir_check)
                 if dir_check.__contains__('7-11-2022') or dir_check.__contains__('6-11-2022'):
-                    ccsv.go(params.N_spheres(),params.final_step(),dir[:len(dir)-1],name_mod='_n10')
+                    ccsv.go(params.N_spheres(), params.final_step(), dir[:len(dir) - 1], name_mod='_n10')
                     print("done")
                     break
             # print(dirs)
